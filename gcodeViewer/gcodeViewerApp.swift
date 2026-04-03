d94d8f462b705615c427cc0a5d6680250759d788
Dirk Clemens <git@adcore.de>
2026-04-03 08:10:24 +0200

    Initial Commit

---
+++ gcodeViewer/gcodeViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  gcodeViewerApp.swift
+//  gcodeViewer
+//
+//  Created by Dirk Clemens on 03.04.26.
+//
+
+import SwiftUI
+
+@main
+struct gcodeViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
